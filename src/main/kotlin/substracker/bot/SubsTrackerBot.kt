3a3f8f42f3faaca533409f5301ddd5494eb47c11
Konstantin Novokreshchenov <knovokresch@yandex-team.ru>
2021-04-19 21:17:45 +0300

    wiremock

--- src/main/kotlin/substracker/bot/SubsTrackerBot.kt
+++ src/main/kotlin/substracker/bot/SubsTrackerBot.kt
@@ -2,13 +2,16 @@ package substracker.bot
 
 import org.springframework.stereotype.Component
 import org.springframework.beans.factory.annotation.Value
+import org.telegram.telegrambots.bots.DefaultBotOptions
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Update
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException
 
 @Component
 class SubsTrackerBot(@Value("\${bot.token}") private val token: String,
-                     @Value("\${bot.username}") private val username: String) : TelegramLongPollingBot() {
+                     @Value("\${bot.username}") private val username: String,
+                     @Value("\${telegram.url}") telegramUrl: String) : TelegramLongPollingBot(defaultBotOptions(telegramUrl)) {
 
     override fun getBotToken(): String = token
 
@@ -23,5 +26,24 @@ class SubsTrackerBot(@Value("\${bot.token}") private val token: String,
                 else -> execute(SendMessage().setChatId(chatId).setText("undefined command"))
             }
         }
+    }
+
+    override fun clearWebhook() {
+        try {
+            super.clearWebhook()
+        } catch (e: TelegramApiRequestException) {
+            // ignore this
+        }
+
+    }
+
+    companion object {
+
+        fun defaultBotOptions(telegramUrl: String): DefaultBotOptions {
+            val options = DefaultBotOptions()
+            options.baseUrl = telegramUrl
+            return options
+        }
+
     }
 }
